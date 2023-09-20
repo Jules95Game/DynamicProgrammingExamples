890ee1a06d8211d6579b2441b4aa805b727b7a67
Jules95Game <romp.julian1@gmail.com>
2023-09-20 15:47:21 +0200

    Added GridTraveler method

---
+++ DynaPrograms.GridTraveler/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DynaPrograms.GridTraveler;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Press enter to start.");
+        Console.ReadLine();
+        Console.WriteLine(GridTraveler(1, 1));
+        Console.WriteLine(GridTraveler(2, 3));
+        Console.WriteLine(GridTraveler(3, 2));
+        Console.WriteLine(GridTraveler(3, 3));
+        Console.WriteLine(GridTraveler(18, 18));
+        Console.Write("Done!");
+        Console.ReadLine();
+    }
+
+    // Memoized
+    // O(m*n) Time
+    // O(n+m) Space
+    private static long GridTraveler(int m, int n, Dictionary<string, long>? memo = null)
+    {
+        memo ??= new();
+        string key = $"{m},{n}";
+        if (memo.ContainsKey(key)) return memo[key];
+        if (m == 1 && n == 1) return 1;
+        if (m == 0 || n == 0) return 0;
+        memo[key] = GridTraveler(m - 1, n, memo) + GridTraveler(m, n - 1, memo);
+        return memo[key];
+    }
+
+    // Unoptimized
+    // O(2^(n+m)) Time
+    // O(n+m) Space
+    private static long BadGridTraveler(int m, int n)
+    {
+        if (m == 1 && n == 1) return 1;
+        if (m == 0 || n == 0) return 0;
+        return BadGridTraveler(m - 1, n) + BadGridTraveler(m, n - 1);
+    }
+}
