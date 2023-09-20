02a2492bf49eaf6a92b5bed279b1b2f29a44680d
Jules95Game <romp.julian1@gmail.com>
2023-09-20 15:22:47 +0200

    Added Fibonacci method

--- DynaPrograms.Fibonacci/Program.cs
+++ DynaPrograms.Fibonacci/Program.cs
@@ -2,8 +2,37 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Press enter to start.");
+        Console.ReadLine();
+        Console.WriteLine(Fib(6));
+        Console.WriteLine(Fib(7));
+        Console.WriteLine(Fib(8));
+        Console.WriteLine(Fib(50));
+        Console.WriteLine(Fib(92));
+        Console.Write("Done!");
+        Console.ReadLine();
+    }
+
+    // Memoized
+    // O(n) Time
+    // O(n) Space
+    private static long Fib(int n, Dictionary<int, long>? memo = null)
+    {
+        memo ??= new();
+        if (memo.ContainsKey(n)) return memo[n];
+        if (n <= 2) return 1;
+        memo[n] = Fib(n - 1, memo) + Fib(n - 2, memo);
+        return memo[n];
+    }
+
+    // Unoptimized
+    // O(2^n) Time
+    // O(n) Space
+    private static long BadFib(int n)
+    {
+        if (n <= 2) return 1;
+        return BadFib(n - 1) + BadFib(n - 2);
     }
 }
