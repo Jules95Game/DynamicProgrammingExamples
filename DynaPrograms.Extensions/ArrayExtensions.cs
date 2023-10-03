f3a050f9acb7b35e274ce134540516b1266a52e6
Jules95Game <romp.julian1@gmail.com>
2023-10-03 12:48:34 +0200

    Added Extension project with ArrayExtensions class

---
+++ DynaPrograms.Extensions/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace DynaPrograms.Extensions;
+
+public static class ArrayExtensions
+{
+    public static string IntArrayToString(this int[]? array)
+    {
+        if (array == null) return "null";
+        if (array == Array.Empty<int>()) return "[]";
+        string output = "[";
+        foreach (int num in array)
+        {
+            output += $" {num},";
+        }
+        output = output.Remove(output.Length - 1);
+        return output += " ]";
+    }
+}
